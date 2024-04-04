9ffa2a71210a8a2c997d026a7c0bec6e4a1526cc
Samuel Johnson <sj8285822@gmail.com>
2024-04-04 15:27:05 +0000

    Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { Routes, Route } from 'react-router-dom'
 
 
@@ -9,9 +8,7 @@ function App() {
 
   return (
     <div>
-      <Routes>
-        
-      </Routes>
+     <h1 className='font-bold flex text-center justify-center text-3xl md:justify-start md:ml-3'>Welcome to my Website</h1>
      
     </div>
     
